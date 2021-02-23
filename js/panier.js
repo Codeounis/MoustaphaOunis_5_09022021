006519133ec4a444463b4a511aa98177687f5da4
Codeounis <park.ounis@gmail.com>
2021-02-23 01:56:21 +0100

    mise en place tableau panier

--- js/panier.js
+++ js/panier.js
@@ -1,11 +1,71 @@
-let teddyPanierJson = localStorage.getItem("listePanier");
-console.log(teddyPanierJson);
-let teddyPanier =JSON.parse(teddyPanierJson);
-console.log(teddyPanier);
+
+
+
+
+
+//  Récupération du Local storage
+
+
+
+let teddyPanier = localStorage.getItem("listePanier");
+    if (teddyPanier != undefined){
+        let Montableaulocalstorage =JSON.parse(teddyPanier);
+        console.log("votre panier est rempli")
+        console.log(Montableaulocalstorage);
+        let leTableauquejeveuxalafin = [];
+        Montableaulocalstorage.map((teddypascomplet,index)=>{
+            fetch("http://localhost:3000/api/teddies/" + teddypascomplet.id).then(function (response){
+                return response.json();
+                }).then(function(teddycomplet){
+                    console.log(teddycomplet);
+                    let indexduteddydansletableauquejeveuxalafin;
+                    leTableauquejeveuxalafin.map((teddy,indexfinal)=>{
+                        if(teddy.id === teddycomplet._id){
+                            indexduteddydansletableauquejeveuxalafin = indexfinal;
+                        }
+                    })
+                    if(indexduteddydansletableauquejeveuxalafin != undefined){
+                        console.log("Produit trouvé");
+                        leTableauquejeveuxalafin[indexduteddydansletableauquejeveuxalafin].quantity += teddypascomplet.quantity;
+                        // couleur supplémentaire
+                      }else{
+                        console.log("Produit non trouvé")
+                        let produit = {id:teddycomplet._id,name:teddycomplet.name,colors:[teddypascomplet.color],quantity:teddypascomplet.quantity,price:teddycomplet.price,image:teddycomplet.imageURL};
+                        leTableauquejeveuxalafin.push(produit);
+                      } 
+                      if(index === Montableaulocalstorage.length -1){
+                          leTableauquejeveuxalafin.map((teddy,indexalafin)=>{
+                              console.log(teddy);
+                            // creer card 
+                          }
+                          )
+                      }
+                    // let teddyRecap = document.createElement("div");
+                    // let teddyName = document.createElement("p");
+                    // teddyRecap.appendChild(teddyName);
+                    // teddyName.className = "itempanier";
+                    // teddyName.innerText = data.name;
+                    // document.getElementById("panier").appendChild(teddyRecap);
+                    
+                })
+            })
+    }else{
+        console.log("Votre panier est vide");    
+    } 
+
+// teddyPanier.map((produit,index)=>{
+     
+//     }
+// }
+// )
+
+
 
 
 // let teddyRecap = document.createElement("div");
 // teddyRecap.className = "itempanier";
+// teddyRecap.innerText = teddyPanier.id
+// console.log(teddyRecap);
 
 // let teddyImg = document.createElement("img");
 // teddyImg.className = "imgTeddy";
